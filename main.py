421d2caa79a624633ab58b9eeb5847ccc5670f30
Dmitri José <dmitrijnferreira@gmail.com>
2025-03-02 23:58:20 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+lista = [-5, -3, -2 ,-1, 0, 1, 2, 3]
+tamanho = len(lista)
+
+for item in lista:
+    if item % 2 == 0:
+        continue
+    print(item)
+
+for i in range(5):
+    print(i)
+
+frutas = ["maçã", "banana", "uva"]
+
+for fruta in frutas:
+	print(fruta)
+
+contador = 0
+
+while (contador < 5):
+	print(contador)
+	contador += 1
